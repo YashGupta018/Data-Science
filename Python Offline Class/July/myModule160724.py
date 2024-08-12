aa1efade06825f5035923e97a8446c4b429dc804
Yash Gupta <ygupta2001@gmail.com>
2024-08-12 12:09:59 +0530

    Custom Python Module 16 July

---
+++ Python Offline Class/July/myModule160724.py
@@ -0,0 +1,14 @@
+var1 = "text1"
+var2 = "text2"
+
+def add():
+    """
+    This add() will return addition of 2 numbers x and y
+    """
+    return 10+20
+
+def mul(x,y):
+    """
+    This mul() will return multiplication of 2 numbers x and y
+    """
+    return x*y
